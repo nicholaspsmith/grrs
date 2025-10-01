d4b262123e29e2142eadfe83ee2be0eef3d6ec2b
nicholaspsmith <npsmith1990@gmail.com>
2025-10-01 14:42:41 -0400

    Working basic search fn

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,12 @@ struct Cli {
 
 fn main() {
     let args = Cli::parse();
-    // let pattern = std::env::args().nth(1).expect("no pattern given");
-    // let path = std::env::args().nth(2).expect("no path given");
     let Cli { pattern, path } = args;
-    println!("pattern: {:?}, path: {:?}", pattern, path)
+    let content = std::fs::read_to_string(&path).expect("could not read file");
+
+    for line in content.lines() {
+        if line.contains(&pattern) {
+            println!("{}", line);
+        }
+    }
 }
