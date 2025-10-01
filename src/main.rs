bec757856be1f5ee5dccb66f1b25d64ba1ac4274
nicholaspsmith <npsmith1990@gmail.com>
2025-10-01 15:18:56 -0400

    Handle error display myself

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,25 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
 
+#[derive(Debug)]
+struct CustomError(String);
+
 #[derive(Parser)]
 struct Cli {
     pattern: String,
     path: std::path::PathBuf,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), CustomError> {
     let args = Cli::parse();
     let Cli { pattern, path } = args;
 
     let file_path = Path::new(&path);
 
     // open file
-    let file = File::open(file_path).unwrap();
+    let file = File::open(file_path)
+        .map_err(|err| CustomError(format!("Error reading `{:?}`: {}", file_path, err)))?;
+
     let reader = BufReader::new(file);
 
     // iterate over lines
@@ -24,7 +29,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let line = match line_result {
             Ok(content) => content,
             Err(error) => {
-                return Err(error.into());
+                return Err(CustomError(format!(
+                    "Error reading file: {:?}",
+                    error.to_string()
+                )));
             }
         };
         if line.contains(&pattern) {
