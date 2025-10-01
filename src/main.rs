f978ca5ed332327303a0bd27e58b71f1aaf48f12
nicholaspsmith <npsmith1990@gmail.com>
2025-10-01 14:54:43 -0400

    Using BufReader to parse file input

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use clap::Parser;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::Path;
 
 #[derive(Parser)]
 struct Cli {
@@ -6,14 +9,23 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
     let Cli { pattern, path } = args;
-    let content = std::fs::read_to_string(&path).expect("could not read file");
 
-    for line in content.lines() {
+    let file_path = Path::new(&path);
+
+    // open file
+    let file = File::open(file_path)?;
+    let reader = BufReader::new(file);
+
+    // iterate over lines
+    for line_result in reader.lines() {
+        let line = line_result?;
         if line.contains(&pattern) {
             println!("{}", line);
         }
     }
+
+    Ok(())
 }
